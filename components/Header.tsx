bc82ecd1ff0a7c336f0c3c8a2836880a1b723af7
Eduard Beller <ieduard.beller@gmail.com>
2023-08-07 17:01:55 +0200

    :sparkles: Update the header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,4 @@
+import { OrganizationSwitcher, SignOutButton, SignedIn } from '@clerk/nextjs';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
@@ -5,11 +6,34 @@ import React from 'react';
 function Header() {
 	return (
 		<header className='topbar'>
-			<nav>
+			<nav className='flex justify-between items-center'>
 				<Link href='/' className='flex items-center gap-4'>
 					<Image src='/assets/logo.svg' alt='logo' width={28} height={28} />
-					<p className='text-heading3-bold text-light-1 max-xs:hidden'>Threads</p>
+					<p className='text-heading3-bold text-light-1 max-xs:hidden'>
+						Threads
+					</p>
 				</Link>
+				<div className='flex items-center gap-1'>
+					<div className='block md:hidden'>
+						<SignedIn>
+							<SignOutButton>
+								<div className='flex cursor-pointer'>
+									<Image
+										src='/assets/logout.svg'
+										alt='logout'
+										width={24}
+										height={24}
+									/>
+								</div>
+							</SignOutButton>
+						</SignedIn>
+					</div>
+					<OrganizationSwitcher  appearance={{
+						elements: {
+							organizationSwitcherTrigger: "py-4 px-4"
+						}
+					}}/>
+				</div>
 			</nav>
 		</header>
 	);
