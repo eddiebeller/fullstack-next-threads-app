86682585f929fc8ab81c5b35aa10fab62caabab2
Eduard Beller <ieduard.beller@gmail.com>
2023-08-07 16:38:57 +0200

    :sparkles: Add assets, add styles to the header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
 
 function Header() {
-  return (
-    <div>Header</div>
-  )
+	return (
+		<header className='topbar'>
+			<nav>
+				<Link href='/' className='flex items-center gap-4'>
+					<Image src='/assets/logo.svg' alt='logo' width={28} height={28} />
+					<p className='text-heading3-bold text-light-1 max-xs:hidden'>Threads</p>
+				</Link>
+			</nav>
+		</header>
+	);
 }
 
-export default Header
+export default Header;
