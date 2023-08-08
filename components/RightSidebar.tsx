20d579a8ecbca705b07934668179bfff67503afc
Eduard Beller <ieduard.beller@gmail.com>
2023-08-08 17:58:19 +0200

    :sparkles: Add markup to the rightsidebar component

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 function RightSidebar() {
-	return <div>RightSidebar</div>;
+	return (
+		<aside className='custom-scrollbar rightsidebar'>
+			<div className='flex flex-1 flex-col justify-start'>
+				<h3>Suggested commitinies</h3>
+			</div>
+		</aside>
+	);
 }
 
 export default RightSidebar;
