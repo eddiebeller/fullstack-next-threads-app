b518ab37f098631223473b2ddb6256489b1d30e5
Eduard Beller <ieduard.beller@gmail.com>
2023-08-08 18:01:56 +0200

    :sparkles: Add basic markup to rightsidebar for now

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -4,7 +4,12 @@ function RightSidebar() {
 	return (
 		<aside className='custom-scrollbar rightsidebar'>
 			<div className='flex flex-1 flex-col justify-start'>
-				<h3>Suggested commitinies</h3>
+				<h3 className='text-heading4-medium text-light-1'>
+					Suggested Communities
+				</h3>
+			</div>
+			<div className='flex flex-1 flex-col justify-start'>
+				<h3 className='text-heading4-medium text-light-1'>Suggested Users</h3>
 			</div>
 		</aside>
 	);
