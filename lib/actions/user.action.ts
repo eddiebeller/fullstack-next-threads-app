4e3e82a01871cc062fcf67de631feefbdbf6dabb
Eduard Beller <ieduard.beller@gmail.com>
2023-08-14 14:19:52 +0200

    :sparkles: Add updateUser backend function

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -1,9 +1,37 @@
 'use server';
 
+import { revalidatePath } from 'next/cache';
 import { connectToDB } from '../mongoose';
+import User from '../models/user.model';
 
-export async function updateUser(): Promise<void> {
+export async function updateUser(
+	userId: string,
+	username: string,
+	name: string,
+	bio: string,
+	image: string,
+	path: string
+): Promise<void> {
 	connectToDB();
 
-	// start make call to the DB
+	try {
+		await User.findByIdAndUpdate(
+			{ id: userId },
+			{
+				username: username.toLowerCase(),
+				name,
+				bio,
+				image,
+				path,
+				onboarded: true,
+			},
+			{ upsert: true }
+		);
+
+		if (path === '/profile/edit') {
+			revalidatePath(path);
+		}
+	} catch (error: any) {
+		throw new Error(`Failed to create/update user: ${error.message}`);
+	}
 }
