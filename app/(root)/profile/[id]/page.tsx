d05e7073e1e51899b6b76fe01fe6bbdb1622d2ac
Eduard Beller <ieduard.beller@gmail.com>
2023-09-13 19:13:24 +0200

    :sparkles: Add tabs to profile

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -3,6 +3,9 @@ import ProfileHeader from '@/components/ProfileHeader';
 import { currentUser } from '@clerk/nextjs';
 import { redirect } from 'next/navigation';
 import { fetchUser } from '@/lib/actions/user.actions';
+import { Tabs, TabsList, TabsContent, TabsTrigger } from '@/components/ui/tabs';
+import { profileTabs } from '@/data';
+import Image from 'next/image';
 
 async function Page({ params }: { params: { id: string } }) {
 	const user = await currentUser();
@@ -22,6 +25,24 @@ async function Page({ params }: { params: { id: string } }) {
 				imageUrl={userInfo.image}
 				bio={userInfo.bio}
 			/>
+			<div className='mt-9'>
+				<Tabs defaultValue='threads' className='w-full'>
+					<TabsList className='tab'>
+						{profileTabs.map((tab) => (
+							<TabsTrigger key={tab.label} value={tab.value} className='tab'>
+								<Image
+									src={tab.icon}
+									alt={tab.label}
+									width={24}
+									height={24}
+									className='object-contain'
+								/>
+								<p className='max-sm:hidden'>{tab.label}</p>
+							</TabsTrigger>
+						))}
+					</TabsList>
+				</Tabs>
+			</div>
 		</section>
 	);
 }
