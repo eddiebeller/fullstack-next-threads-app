a6518848728f5d2031cb5a1e53816912e70e7a91
Eduard Beller <ieduard.beller@gmail.com>
2023-09-18 17:12:14 +0200

    :sparkles: Show user on search page

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -2,9 +2,6 @@ import React from 'react';
 import { currentUser } from '@clerk/nextjs';
 import { redirect } from 'next/navigation';
 import { fetchUser, fetchUsers } from '@/lib/actions/user.actions';
-import { profileTabs } from '@/data';
-import Image from 'next/image';
-import Threadstab from '@/components/Threadstab';
 
 async function Page() {
 	const user = await currentUser();
@@ -12,24 +9,38 @@ async function Page() {
 
 	// Fech current user
 	const userInfo = await fetchUser(user.id);
+
+	if (!userInfo?.onboarded) redirect('/onboarding');
+
 	// Fetch users
 	const result = await fetchUsers({
 		userId: user.id,
 		searchString: '',
-		pageNumber: 25,
-		pageSize: 1,
+		pageNumber: 1,
+		pageSize: 25,
 	});
 
-	if (!userInfo?.onboarded) redirect('/');
+	return (
+		<section>
+			<h1 className='head-text mb-10'>Search</h1>
 
-	// TODO: Fetch users
-	async function Page() {
-		return (
-			<section>
-				<h1 className='head-text mb-10'>Search</h1>
-			</section>
-		);
-	}
+			{/* Searchbar component here */}
+
+			<div className='mt-14 flex flex-col gap-9'>
+				{result?.users.length === 0 ? (
+					<p className='no-result'>No users founded...</p>
+				) : (
+					<>
+						{result?.users.map((user) => (
+							<p className='text-white text-heading1-bold'>
+								{user.name} {user.username}
+							</p>
+						))}
+					</>
+				)}
+			</div>
+		</section>
+	);
 }
 
 export default Page;
