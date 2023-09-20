c5a085c1f1eb8413a2bea430df68b819d90f90c7
Eduard Beller <ieduard.beller@gmail.com>
2023-09-20 15:44:30 +0200

    :sparkles: Start activities

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
+import { currentUser } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
+import { fetchUser } from '@/lib/actions/user.actions';
 
 async function Page() {
+	const user = await currentUser();
+	if (!user) return null;
+
+	// Fech current user
+	const userInfo = await fetchUser(user.id);
+	if (!userInfo?.onboarded) redirect('/onboarding');
+
+	// TODO: create a new action fetchActivities() or getNotifacations()
+
 	return (
 		<section>
 			<h1 className='head-text mb-10'>Activity</h1>
