0db2af4222ad3a4f598fa6f7ebbfa22e0a89901f
Eduard Beller <ieduard.beller@gmail.com>
2023-10-05 14:08:48 +0200

    :sparkles: Show list of communities

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -1,9 +1,43 @@
 import React from 'react';
+import { currentUser } from '@clerk/nextjs';
+import UserCard from '@/components/UserCard';
+import { fetchCommunities } from '@/lib/actions/community.actions';
 
 async function Page() {
+	const user = await currentUser();
+	if (!user) return null;
+
+	// Fetch communities
+	const result = await fetchCommunities({
+		searchString: '',
+		pageNumber: 1,
+		pageSize: 25,
+	});
+
 	return (
 		<section>
-			<h1 className='head-text mb-10'>Communities</h1>
+			<h1 className='head-text mb-10'>Search</h1>
+
+			{/* Searchbar component here */}
+
+			<div className='mt-14 flex flex-col gap-9'>
+				{result?.communities.length === 0 ? (
+					<p className='no-result'>No communities founded...</p>
+				) : (
+					<>
+						{result?.communities.map((community) => (
+							<UserCard
+								key={community.id}
+								id={community.id}
+								name={community.name}
+								username={community.username}
+								image={community.image}
+								personType='Community'
+							/>
+						))}
+					</>
+				)}
+			</div>
 		</section>
 	);
 }
